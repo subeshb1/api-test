33ddafaaea1c99c93cb1dc656fa2e793502e0208
Subesh Bhandari <subeshb1@gmail.com>
2020-05-24 19:40:33 +0545

    Add contains eq and has key methods

--- api-test.sh
+++ api-test.sh
@@ -171,14 +171,82 @@ api_factory() {
 
 test_factory() {
   for TEST_CASE in $@; do
+    API_ERROR=0
     echo "${BOLD}Testing Case:${RESET} $TEST_CASE"
     echo_v "${BOLD}Description: ${RESET}$(jq -r ".testCases.$TEST_CASE.description" $FILE)"
     echo_v "${BOLD}Action: ${RESET}$(jq -r ".testCases.$TEST_CASE.method //\"GET\" | ascii_upcase" $FILE) $(jq -r ".testCases.$TEST_CASE.path" $FILE)"
     call_api $TEST_CASE
+    if [[ $API_ERROR == 1 ]]; then
+      return
+    fi
+    # echo "$RESPONSE_HEADER"
+    # Has Key
+    # jq --argjson header "$RESPONSE_HEADER" ".testCases.$TEST_CASE.test.header.contains | keys as \$check_key | \$header | keys - \$check_key | map([.]) |. as \$arr | \$header | delpaths(\$arr)" $FILE
 
+    HEADER_CONTAINS=$(jq -r ".testCases.$TEST_CASE.test.header.hasKey" $FILE)
+    EADER_HAS_KEY=$(jq  ".testCases.$TEST_CASE.test.header.hasKey | .[]" $FILE)
+    echo $EADER_HAS_KEY
+    # contains "$HEADER_CONTAINS" "$RESPONSE_HEADER"
+    has_key "$EADER_HAS_KEY" "$RESPONSE_HEADER"
+    # jq --arg header "$RESPONSE_HEADER" ".testCases.$TEST_CASE.test.header.contains as \$check | \
+    #  \$header | fromjson as \$header |
+    #  \$check | keys as \$check_key |
+    #  \$header | keys  |
+    #  if contains(\$check_key) then
+    #   \$check
+    #  else false
+    #  end " $FILE
+
+    # BODY_EQ=$(jq -r ".testCases.$TEST_CASE.test.body.eq" $FILE)
+    # check_eq "$BODY_EQ" "$RESPONSE_BODY"
+
+    # BODY_EQ=$(jq -r ".testCases.$TEST_CASE.test.body.eq" $FILE)
+    # check_eq "$BODY_EQ" "$RESPONSE_BODY"
     echo ""
     echo ""
   done
+}
+
+contains() {
+  jq --argjson a "$1" --argjson b "$2" -n '$b | contains($a)'
+  # local common=$(jq --argjson a "$1" --argjson b "$2" -n '$a | select( length != 0) | keys as $check_key | $b | keys - $check_key | map([.]) | . as $arr | $b | delpaths($arr) | if . != {} then . else null end')
+
+  # if [ -z "$common" ]; then
+  #   echo "\$common is NULL"
+  #   return 1
+  # fi
+  # check_eq "$1" "$common"
+}
+
+has_key() {
+  # jq -argjson a "$1" --argjson b "$2" -n '$b | keys as $check_key | $header | keys  | contains($check_key)'
+  local paths=$(jq 'def path2text($value):
+  def tos: if type == "number" then . else "\"\(tojson)\"" end;
+  reduce .[] as $segment ("";  .
+    + ($segment
+       | if type == "string" then "." + . else "[\(.)]" end));
+
+
+paths(scalars) as $p
+  | getpath($p) as $v
+  | $p | path2text($v)' <<<"$2")
+  for path in $1; do
+    for data_path in $paths; do
+      local FOUND=0
+      if [[ $path == $data_path ]]; then
+        echo "FOUND: $path"
+        FOUND=1
+        break
+      fi
+    done
+
+    if [[ $FOUND == 0 ]]; then
+     echo "NOT FOUND: $path"
+    fi
+  done
+}
+check_eq() {
+  jq --argjson a "$1" --argjson b "$2" -n "def post_recurse(f): def r: (f | select(. != null) | r), .; r; def post_recurse: post_recurse(.[]?); (\$a | (post_recurse | arrays) |= sort) as \$a | (\$b | (post_recurse | arrays) |= sort) as \$b | \$a == \$b"
 }
 
 run() {
